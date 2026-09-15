c7e8c70db073fffa5ae5cbab8a9d6356b5c1bdb4
Miguel <spiwskyn0@gmail.com>
2026-09-15 14:29:14 -0300

    Adicionando botões Novo, Remover e Atualizar

--- main.py
+++ main.py
@@ -32,3 +32,14 @@ frame_e_cima.grid(row=0, column=0, sticky=NSEW)
 
 frame_e_baixo = Frame(frame_esquerda, width=300, height=150, bg=co2, relief=RAISED)
 frame_e_baixo.grid(row=1, column=0, sticky=NSEW)
+
+############### Criando os Botões ###############
+
+b_novo = Button(frame_e_cima, text="Novo", width=10, height=1, bg=co3, fg="white", font="5", anchor="center", relief=RAISED)
+b_novo.grid(row=0, column=0, sticky=NSEW, pady=1)
+
+b_remover = Button(frame_e_cima, text="Remover", width=10, height=1, bg=co4, fg="white", font="5", anchor="center", relief=RAISED)
+b_remover.grid(row=0, column=1, sticky=NSEW, pady=1)
+
+b_atualizar = Button(frame_e_cima, text="Atualizar", width=10, height=1, bg=co5, fg="white", font="5", anchor="center", relief=RAISED)
+b_atualizar.grid(row=0, column=2, sticky=NSEW, pady=1)
